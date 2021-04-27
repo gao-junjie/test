0b71f5e0a769fdfdb5131660103a4b52406f3447
gao-junjie <1960257252@qq.com>
2021-04-27 21:21:03 +0800

    First Commit

---
+++ ttttest/ttttest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ttttest
+//
+//  Created by mac on 2021/4/27.
+//  Copyright © 2021 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
